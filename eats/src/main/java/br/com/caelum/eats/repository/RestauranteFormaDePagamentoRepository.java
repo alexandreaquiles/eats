67b28682105e4de679406f13fc039187f4a191a8
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2019-04-15 17:01:06 -0300

    detalhe

--- eats/src/main/java/br/com/caelum/eats/repository/RestauranteFormaDePagamentoRepository.java
+++ eats/src/main/java/br/com/caelum/eats/repository/RestauranteFormaDePagamentoRepository.java
@@ -8,9 +8,8 @@ import org.springframework.data.jpa.repository.Query;
 import br.com.caelum.eats.model.FormaDePagamento;
 import br.com.caelum.eats.model.Restaurante;
 import br.com.caelum.eats.model.RestauranteFormaDePagamento;
-import br.com.caelum.eats.model.RestauranteFormaDePagamento.RestauranteFormaDePagamentoId;
 
-public interface RestauranteFormaDePagamentoRepository extends JpaRepository<RestauranteFormaDePagamento, RestauranteFormaDePagamentoId> {
+public interface RestauranteFormaDePagamentoRepository extends JpaRepository<RestauranteFormaDePagamento, RestauranteFormaDePagamento.RestauranteFormaDePagamentoId> {
 
 	@Query("select f from RestauranteFormaDePagamento rf join rf.restaurante r join rf.formaDePagamento f where r = :restaurante order by f.nome")
 	List<FormaDePagamento> findAllByRestauranteOrderByNomeAsc(Restaurante restaurante);
