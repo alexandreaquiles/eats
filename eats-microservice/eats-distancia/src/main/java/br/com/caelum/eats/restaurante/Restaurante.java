124d61404d62c49564ae0459fc596c309cb9fff7
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2019-04-20 17:09:49 -0300

    enxuga Restaurante

--- eats-microservice/eats-distancia/src/main/java/br/com/caelum/eats/restaurante/Restaurante.java
+++ eats-microservice/eats-distancia/src/main/java/br/com/caelum/eats/restaurante/Restaurante.java
@@ -1,16 +1,9 @@
 package br.com.caelum.eats.restaurante;
 
-import java.math.BigDecimal;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Positive;
-import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -22,35 +15,14 @@ import lombok.NoArgsConstructor;
 @Data
 public class Restaurante {
 
-	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@NotBlank @Size(max=18)
-	private String cnpj;
-	
-	@NotBlank @Size(max=255)
-	private String nome;
-	
-	@Size(max=1000)
-	private String descricao;
-
-	@NotBlank @Size(max=9)
 	private String cep;
 
-	@NotBlank @Size(max=300)
-	private String endereco;
-
-	@Positive
-	private BigDecimal taxaDeEntregaEmReais;
-	
-	@Positive @Min(10) @Max(180)
-	private Integer tempoDeEntregaMinimoEmMinutos;
-	
-	@Positive @Min(10) @Max(180)
-	private Integer tempoDeEntregaMaximoEmMinutos;
-	
 	private Boolean aprovado;
 
 	private Long tipoDeCozinhaId;
-	
+
 }
