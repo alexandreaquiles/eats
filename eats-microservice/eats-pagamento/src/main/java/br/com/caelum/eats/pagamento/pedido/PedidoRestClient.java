4e60d2e3d1f7db90a163f0569cbe32a4de78d11f
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2019-04-23 15:40:42 -0300

    [REST Client] Migrando Rest cliente de pagamento a pedido para usar OpenFeign. Filé!

--- eats-microservice/eats-pagamento/src/main/java/br/com/caelum/eats/pagamento/pedido/PedidoRestClient.java
+++ eats-microservice/eats-pagamento/src/main/java/br/com/caelum/eats/pagamento/pedido/PedidoRestClient.java
@@ -1,29 +1,17 @@
 package br.com.caelum.eats.pagamento.pedido;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
-@Service
-public class PedidoRestClient {
+@FeignClient(url="${configuracao.pedido.service.url}", name="pedido")
+public interface PedidoRestClient {
 
-	private String pedidoServiceUrl;
-	private RestTemplate restTemplate;
+	@GetMapping("/pedidos/{pedidoId}")
+	public PedidoDto detalhaPorId(@PathVariable("pedidoId") Long pedidoId);
 
-	public PedidoRestClient(RestTemplateBuilder restTemplateBuilder, @Value("${configuracao.pedido.service.url}") String pedidoServiceUrl) {
-		this.pedidoServiceUrl = pedidoServiceUrl;
-		this.restTemplate = restTemplateBuilder.build();
-	}
-	
-	public PedidoDto detalhaPorId(Long pedidoId) {
-		String url = pedidoServiceUrl + "/pedidos/" + pedidoId;
-		return restTemplate.getForObject(url, PedidoDto.class);
-	}
-
-	public void avisaQueFoiPago(Long pedidoId) {
-		String url = pedidoServiceUrl + "/pedidos/" + pedidoId + "/pago";
-		restTemplate.put(url, null);
-	}
+	@PutMapping("/pedidos/{pedidoId}/pago")
+	public void avisaQueFoiPago(@PathVariable("pedidoId") Long pedidoId);
 
 }
