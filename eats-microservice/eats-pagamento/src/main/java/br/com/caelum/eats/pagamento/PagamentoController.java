bed68086af5d95a76562b4c90e299ea6027d9eaa
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2019-04-24 15:46:32 -0300

    [REST] Pagamento com 201 Created.

--- eats-microservice/eats-pagamento/src/main/java/br/com/caelum/eats/pagamento/PagamentoController.java
+++ eats-microservice/eats-pagamento/src/main/java/br/com/caelum/eats/pagamento/PagamentoController.java
@@ -1,12 +1,21 @@
 package br.com.caelum.eats.pagamento;
 
+import java.net.URI;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.caelum.eats.exception.ResourceNotFoundException;
 import br.com.caelum.eats.pagamento.pedido.PedidoDto;
 import br.com.caelum.eats.pagamento.pedido.PedidoRestClient;
 
@@ -23,12 +32,20 @@ public class PagamentoController {
 		this.pedidoClient = pedidoClient;
 		this.notaFiscal = notaFiscal;
 	}
+	
+	@GetMapping("/{id}")
+	public PagamentoDto detalha(@PathVariable("id") Long id) {
+		Pagamento pagamento = pagamentoRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException());
+		return new PagamentoDto(pagamento);
+	}
 
 	@PostMapping
-	public PagamentoDto cria(@RequestBody Pagamento pagamento) {
+	public ResponseEntity<PagamentoDto> cria(@RequestBody Pagamento pagamento, UriComponentsBuilder uriBuilder) {
 		pagamento.setStatus(Pagamento.Status.CRIADO);
 		Pagamento salvo = pagamentoRepo.save(pagamento);
-		return new PagamentoDto(salvo);
+		UriComponents uriComponents = uriBuilder.path("/pagamentos/{id}").buildAndExpand(salvo.getId());
+		URI uri = uriComponents.toUri();
+		return ResponseEntity.created(uri).contentType(MediaType.APPLICATION_JSON).body(new PagamentoDto(salvo));
 	}
 
 	@PutMapping("/{id}")
