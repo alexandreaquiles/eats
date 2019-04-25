ae16120300397c2e0d3bb4a7c7b7868ebee7d79f
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2019-04-25 12:36:12 -0300

    [ApiGateway] EXTRA: custom filter pra Rate Limiting. Permite 1 request a cada 30 segundos.

---
+++ eats-microservice/api-gateway/src/main/java/br/com/caelum/apigateway/RateLimitingZuulFilter.java
@@ -0,0 +1,53 @@
+package br.com.caelum.apigateway;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ReflectionUtils;
+
+import com.google.common.util.concurrent.RateLimiter;
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+@Component
+public class RateLimitingZuulFilter extends ZuulFilter {
+
+	private final RateLimiter rateLimiter = RateLimiter.create(1.0 / 30.0); //queries per second
+
+	@Override
+	public String filterType() {
+		return FilterConstants.PRE_TYPE;
+	}
+
+	@Override
+	public int filterOrder() {
+		return Ordered.HIGHEST_PRECEDENCE + 100;
+	}
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() {
+		try {
+			RequestContext currentContext = RequestContext.getCurrentContext();
+			HttpServletResponse response = currentContext.getResponse();
+
+			if (!this.rateLimiter.tryAcquire()) {
+				response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
+				response.getWriter().append(HttpStatus.TOO_MANY_REQUESTS.getReasonPhrase());
+				currentContext.setSendZuulResponse(false);
+			}
+		} catch (IOException e) {
+			ReflectionUtils.rethrowRuntimeException(e);
+		}
+		return null;
+	}
+}
