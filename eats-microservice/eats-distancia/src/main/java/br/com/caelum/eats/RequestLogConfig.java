27511bc124d706f984590652d9ed5048e0c65498
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2019-04-29 16:42:57 -0300

    [ClientSideLoadBalancing] Loga todos os requests de eats-distancia

---
+++ eats-microservice/eats-distancia/src/main/java/br/com/caelum/eats/RequestLogConfig.java
@@ -0,0 +1,19 @@
+package br.com.caelum.eats;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLogConfig {
+
+	@Bean
+	public CommonsRequestLoggingFilter requestLoggingFilter() {
+	    CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+	    loggingFilter.setIncludeClientInfo(true);
+	    loggingFilter.setIncludeQueryString(true);
+	    loggingFilter.setIncludePayload(true);
+	    return loggingFilter;
+	}
+
+}
