95129b764e94d9150e9027483b09bf4394faba7b
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2019-05-02 17:25:36 -0300

    [CircuitBreaker - 2] @HystrixCommand

--- eats-microservice/api-gateway/src/main/java/br/com/caelum/apigateway/DistanciaRestClient.java
+++ eats-microservice/api-gateway/src/main/java/br/com/caelum/apigateway/DistanciaRestClient.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @Service
 public class DistanciaRestClient {
 
@@ -15,6 +17,7 @@ public class DistanciaRestClient {
 		this.restTemplate = restTemplate;
 	}
 
+	@HystrixCommand
 	public RestauranteComDistanciaDto porCepEId(String cep, Long restauranteId) {
 		String url = distanciaServiceUrl+"/restaurantes/"+cep+"/restaurante/"+restauranteId;
 		return restTemplate.getForObject(url, RestauranteComDistanciaDto.class);
