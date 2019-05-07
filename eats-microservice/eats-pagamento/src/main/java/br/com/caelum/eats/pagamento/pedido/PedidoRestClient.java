a0a2cad4b833c94e92677897476e17f8e3b285fa
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2019-05-07 18:36:02 -0300

    [ServiceRegistry] Configura Feign para usar name registrado no Eureka.

--- eats-microservice/eats-pagamento/src/main/java/br/com/caelum/eats/pagamento/pedido/PedidoRestClient.java
+++ eats-microservice/eats-pagamento/src/main/java/br/com/caelum/eats/pagamento/pedido/PedidoRestClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
-@FeignClient(url="${configuracao.pedido.service.url}", name="pedido")
+@FeignClient(name="monolito")
 public interface PedidoRestClient {
 
 	@GetMapping("/pedidos/{pedidoId}")
