310b580d81eed0be618112fa272d09a535edeac1
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2019-05-15 15:45:21 -0300

    [ExternalConfiguration] Tentando fazer refresh de configurações com o Spring Cloud Bus. Criei uma propriedade de aliquota e um controller no projeto eats-nota-fiscal. Porém, NÃO FUNCIONOU. Só são recarregadas as propriedades ao reiniciar o serviço.

---
+++ eats-microservice/eats-nota-fiscal/src/main/java/br/com/caelum/notafiscal/AliquotaController.java
@@ -0,0 +1,20 @@
+package br.com.caelum.notafiscal;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class AliquotaController {
+	
+	@Value("${configuracao.aliquota}")
+	private double aliquota;
+	
+	@GetMapping("/aliquota")
+	public double obtemAliquota() {
+		return aliquota;
+	}
+
+}
