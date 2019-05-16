9896083b34e7b77c71f478b26ac48ec127ee016c
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2019-05-16 18:48:55 -0300

    [Monitoramento] Configura Turbine Stream, evitando ter que explicitar nomes dos serviços cujos circuit breakers serão agregados

--- eats-microservice/turbine/src/main/java/br/com/caelum/turbine/TurbineApplication.java
+++ eats-microservice/turbine/src/main/java/br/com/caelum/turbine/TurbineApplication.java
@@ -2,11 +2,9 @@ package br.com.caelum.turbine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
-@EnableTurbine
-@EnableDiscoveryClient
+@EnableTurbineStream
 @SpringBootApplication
 public class TurbineApplication {
 
